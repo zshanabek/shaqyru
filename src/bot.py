5c7a981fab7079cc8a393b189f3e4246d05d092a
Zhunisali <zshanabek@gmail.com>
2020-06-10 01:22:03 +0600

    add role

--- src/bot.py
+++ src/bot.py
@@ -1,20 +1,45 @@
+from sqligther import SQLighter
 import config
 import telebot
+from telebot import types
 import logging
+import os
 
 bot = telebot.TeleBot(config.token)
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
+db = SQLighter(config.database_name)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+    bot.send_video(message.chat.id, config.video_id, None)
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add('Нет', 'Да')
+    msg = bot.reply_to(message, 'Хотите знать больше?', reply_markup=markup)
+    bot.register_next_step_handler(msg, process_choice_step)
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+def process_choice_step(message):
+    chat_id = message.chat.id
+    answer = message.text
+    if (answer == u'Да'):
+        positive_answer(message)
+
+    elif (answer == u'Нет'):
+        negative_answer(message)
+
+
+def positive_answer(message):
+    chat_id = message.chat.id
+    bot.send_message(
+        chat_id, f"Дорогой гость, я приглашаю тебя в чат: {config.invite_link}")
+
+
+def negative_answer(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Ок, скоро мы вернемся")
 
 
 bot.polling(none_stop=False, interval=0, timeout=20)
--- src/config.py
+++ src/config.py
@@ -1,1 +1,4 @@
 token = "1217988314:AAFzC6WYtQZUw1ZMdlfU0zE_3OKX0V3weI0"
+video_id = "BAACAgIAAxkDAAMWXt_AyjRDILjidvjH_nQ3hhsiQnAAAlYGAAJIoAABS3-T7oCbOvwGGgQ"
+database_name = 'shaqyru.db'
+invite_link = 'https://t.me/joinchat/QgJJsVYyxAtZCCyI2vvcNQ'

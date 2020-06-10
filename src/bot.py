8343e1dde8fdea0e6fc23e1ccfa3477898df7f74
Zhunisali <zshanabek@gmail.com>
2020-06-10 09:37:48 +0600

    refactor

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,6 @@ import config
 import telebot
 from telebot import types
 import logging
-import os
 
 bot = telebot.TeleBot(config.token)
 logger = telebot.logger
@@ -13,33 +12,23 @@ db = SQLighter(config.database_name)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
-    bot.send_video(message.chat.id, config.video_id, None)
+    msg = bot.send_video(message.chat.id, config.video_id, None)
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.add('Нет', 'Да')
-    msg = bot.reply_to(message, 'Хотите знать больше?', reply_markup=markup)
+    bot.reply_to(msg, 'Хотите знать больше?', reply_markup=markup)
     bot.register_next_step_handler(msg, process_choice_step)
 
 
 def process_choice_step(message):
     chat_id = message.chat.id
     answer = message.text
+    decision = ""
     if (answer == u'Да'):
-        positive_answer(message)
-
+        decision = f"Дорогой гость, я приглашаю тебя в чат: {config.invite_link}"
     elif (answer == u'Нет'):
-        negative_answer(message)
+        decision = "Ок, скоро мы вернемся"
+    markup = types.ReplyKeyboardRemove(selective=False)
+    bot.send_message(chat_id, decision, reply_markup=markup)
 
 
-def positive_answer(message):
-    chat_id = message.chat.id
-    bot.send_message(
-        chat_id, f"Дорогой гость, я приглашаю тебя в чат: {config.invite_link}")
-
-
-def negative_answer(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, "Ок, скоро мы вернемся")
-
-
-bot.polling(none_stop=False, interval=0, timeout=20)
+bot.polling()
